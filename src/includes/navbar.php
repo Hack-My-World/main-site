fd7a694179b73ae4777b192786573f98b53e72fc
kablaa <kablaa@Tmac.local>
2017-06-02 00:42:52 -0400

    updated contact functionality

--- src/includes/navbar.php
+++ src/includes/navbar.php
@@ -17,7 +17,7 @@ function getNavbar($depth){
                     <span class='icon-bar'></span>
                     <span class='icon-bar'></span>
                 </button>
-                <a class='navbar-brand' href='https://keybase.io/kablaa'>Contact</a>
+                <a class='navbar-brand' href='mailto:kablaa@hackmy.world'>Contact</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
