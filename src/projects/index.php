6b9e0db54c6e97573bceb10f378cb695fa6be4c2
tlukasiewicz <tyler.lukasiewicz@nccgroup.trust>
2018-10-15 09:23:10 -0500

    added wasm whitepaper

--- src/projects/index.php
+++ src/projects/index.php
@@ -36,6 +36,15 @@
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <p>
                 <div class="post-preview">
+                    <a href="https://i.blackhat.com/us-18/Thu-August-9/us-18-Lukasiewicz-WebAssembly-A-New-World-of-Native_Exploits-On-The-Web-wp.pdf">
+                        <h2 class="post-title">
+                            WebAssembly Research
+                        </h2>
+                        <h3 class="post-sub-title">September, 2018</h3>
+                    </a>
+                </div>
+                <hr>
+                <div class="post-preview">
                     <a href="https://youtu.be/75gBFiFtAb8">
                         <h2 class="post-title">
                             x86 Assembly Crash Course Video
